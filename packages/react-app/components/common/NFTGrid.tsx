98946dedcacc0c841648c4a9876b070240c1dbcf
Sifan23 <sifanyg123@gmail.com>
2024-04-10 10:53:20 +0300

    nft grid

---
+++ packages/react-app/components/common/NFTGrid.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  nfts: string[];
+};
+
+function NFTGrid({ nfts }: Props) {
+  return (
+    <>
+      <div className="grid grid-cols-2 md-grid-cols-3 gap-4">
+        {nfts.map((nft, index) => {
+          return (
+            <div key={index}>
+              <img className="h-auto max-w-full rounded-lg" src={nft} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default NFTGrid;
